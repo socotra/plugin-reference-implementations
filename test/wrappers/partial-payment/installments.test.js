e4090146ea041e925459e866a8083b3b0b2309c4
Mark Szymanski <mark.szymanski@socotra.com>
2022-09-09 10:50:10 -0400

    mismatched (aka partial) payments tests for over, underpay

--- test/wrappers/partial-payment/installments.test.js
+++ test/wrappers/partial-payment/installments.test.js
@@ -2,31 +2,40 @@ const { createInstallments } = require('../../../scripts/wrappers/partial-paymen
 const { getNoOpPaymentScheduleChange } = require('../../sample-data/paymentScheduleSampleData.js').specialCaseSamples;
 const { commonAssertions } = require('../../test-helpers.js');
 
-// testing mock
-global.socotraApi = {
-    getAuxData: function() {
-        return JSON.stringify({
-            paymentAmount: '150.00',
-            targetInvoiceLocator: '0bffe358-eb80-4214-820e-c605b4ae4f11',
-            expires: 9999793584863
-        });
-    },
-    deleteAuxData: function() {}
-};
+const EPSILON = 0.000001;
+const TARGET_INVOICE_LOCATOR = '0bffe358-eb80-4214-820e-c605b4ae4f11';
 
 describe('partial payments implementation', () => {
-    const { installments } = createInstallments(getNoOpPaymentScheduleChange());
-    const originalData = getNoOpPaymentScheduleChange();
+    describe.each(['30.00', '1000.00'])('client paid %i', (clientPaidAmount) => {
+        global.socotraApi = {
+            getAuxData: function() {
+                return JSON.stringify({
+                    paymentAmount: clientPaidAmount,
+                    targetInvoiceLocator: TARGET_INVOICE_LOCATOR,
+                    expires: 9999793584863
+                });
+            },
+            deleteAuxData: function() {}
+        };
 
-    it('should return expected values', () => {
-        const { installments }= createInstallments(getNoOpPaymentScheduleChange());
-        expect(installments).toHaveLength(3);
-        expect(installments[0].invoiceItems).toHaveLength(1);
-        expect(installments[0].invoiceItems[0].chargeId).toEqual('2_premium_db52d335-1181-4e17-8c1e-e1dd12adf48f');
-        expect(installments[1].invoiceItems).toHaveLength(1);
-        expect(installments[1].invoiceItems[0].chargeId).toEqual('2_premium_db52d335-1181-4e17-8c1e-e1dd12adf48f');
-        expect(installments[2].invoiceItems).toHaveLength(4);
+        const { installments } = createInstallments(getNoOpPaymentScheduleChange());
+        const originalData = getNoOpPaymentScheduleChange();
+
+        it('should have an initial installment with a single charge whose amount is the reverse' +
+            ' of the mismatch invoice original amount', () => {
+            const reversalInvoiceItems = installments[0].invoiceItems;
+            const mismatchInvoiceAmount = originalData.policy.invoices.find(i => i.locator === TARGET_INVOICE_LOCATOR).totalDue;
+            expect(reversalInvoiceItems).toHaveLength(1);
+            expect(reversalInvoiceItems[0].amount + parseFloat(mismatchInvoiceAmount)).toBeLessThan(EPSILON);
+        });
+
+        it('should have a second installment with a charge for the amount the client actually paid', () => {
+            const offsetChargeId = installments[0].invoiceItems[0].chargeId;
+            const offsetChargeForClientAmount = installments[1].invoiceItems.filter(i => i.chargeId == offsetChargeId);
+            expect(offsetChargeForClientAmount).toHaveLength(1);
+            expect(offsetChargeForClientAmount[0].amount - parseFloat(clientPaidAmount)).toBeLessThan(EPSILON);
+        });
+
+        commonAssertions(originalData, installments);
     });
-
-    commonAssertions(originalData, installments);
 });
